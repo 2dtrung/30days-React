9bf295feda957f0218ed3647aa5e9959595eb60c
Duc Trung <sductrung@gmail.com>
2021-08-28 17:48:12 +0700

    complete project to show number and color using map

--- src/index.js
+++ src/index.js
@@ -1,58 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Intro = () => (
-  <div>
-    <h1>SUBSCRIBE</h1>
-    <p>Sign up with your email to receive news and updates</p>
-  </div>
-)
+function isEven(num) {
+  if (num%2 === 0)
+    return true
+  else return false
+}
+function isPrime(num) {
+  if (num <= 1) return false
+  for(var n = 2; n <= num/2;n++) {
+    if(num%n === 0)
+      return false
+  }
+  return true
+}
 
-const InputArea = ({data: {firstInput, secondInput, thirdInput}}) => {
+const listNum = () => {
+  let list = [];
+  for (var i =0; i <= 31; i++) {
+    if (isPrime(i)) {
+      list.push({value: i,color: 'rgb(253,94,83)'})
+    }
+    else if (isEven(i)) list.push({value: i,color: 'rgb(33,191,115)'})
+    else list.push({value: i,color: 'rgb(253,219,58)'})
+  }
+  return list;
+}
+
+const NumberContainer = ({number:{value,color}}) => {
+  const style = {
+    backgroundColor: color
+  }
   return (
-    <div className='input-area'>
-      <Input inValue={firstInput}/>
-      <Input inValue={secondInput}/>
-      <Input inValue={thirdInput}/>
+    <div className='number-container' style={style}>
+      <p>{value}</p>
     </div>
   );
-
 }
 
-const Input = ({inValue}) => (
-  <input type='text' placeholder={inValue}/>
-)
-
-const Subcribe = ({value}) => {
+const TableNumber = ({listNum}) => {
+  const listNumber = listNum.map(number => <NumberContainer key={number.value} number={number}/>)
   return (
-    <button class='button' type='submit'>{value}</button>
+    <div className="table-number">
+      {listNumber}
+    </div>
   );
-
 }
 
-
-const Main = ({titleText,}) => {
-  const data = {
-    firstInput: 'First name',
-    secondInput: 'Last name',
-    thirdInput: 'Email'
-  }
-  const btn = 'Subscribe'
+const Intro = () => {
   return (
-    <div>
+    <div className='intro-part'>
+      <h1>30 Days Of React</h1>
+      <h2>Number Generator</h2>
+    </div>
+  );
+}
+
+const Wrapper = () => {
+  return (
+    <div className='wrapper'>
       <Intro />
-      <InputArea data={data} />
-      <Subcribe value={btn} />
+      <TableNumber listNum={listNum()}/>
     </div>
   );
 }
 
 const App = () => (
-  <div className="container"> 
-    <Main />
+  <div>
+    <Wrapper />
   </div>
 )
-
 const rootElement = document.getElementById('root')
 // we render the App component using the ReactDOM package
 ReactDOM.render(<App />, rootElement)
